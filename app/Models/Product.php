cf1102e60656e93acedd17fc7e8f318964745287
K-Thaonanthakouman <khonsavant.thao-nantha-kouman@le-campus-numerique.fr>
2026-02-05 14:03:01 +0100

    P4E1

---
+++ app/Models/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'description',
+        'image',
+        'price',
+        'stock',
+        'active'
+    ];
+
+    protected $hidden = [];
+    
+    protected $casts = [
+        'price' => 'decimal:2',
+        'active' => 'boolean'
+    ];
+}
